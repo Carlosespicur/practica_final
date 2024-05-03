caf3b4e46799c254b9aceaa89469105de2a720ac
Carlosespicur <carlosespicur@proton.me>
2024-05-03 11:37:22 +0200

    Added backend archives

---
+++ backend/PracticaFinal/Answer.cs
@@ -0,0 +1,19 @@
+﻿namespace PracticaFinal
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public string Text { get; set; }
+        public bool IsCorrect { get; set; }
+
+        public Answer(int questionId, string text, bool isCorrect, int id = 0)
+        {
+            this.Id = id;
+            this.QuestionId = questionId;
+            this.Text = text;
+            this.IsCorrect = isCorrect;
+
+        }
+    }
+}
